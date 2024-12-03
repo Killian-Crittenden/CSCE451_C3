2549d48f03e9f31f31869e8ce04dda2e775bf7d7
AjNuk <anuccitelli2@LAPTOP-JFADIBA6.localdomain>
2024-12-03 15:51:05 -0600

    basic file watchers

--- C3.py
+++ C3.py
@@ -1,1 +1,25 @@
+import sys
+import subprocess
+
+num_args = len(sys.argv)
+
+if (num_args != 2):
+    print("Error incorrect args")
+    exit()
+
+dir_name = sys.argv[1]
+ 
+process = subprocess.Popen(["inotifywait", "-m", dir_name], stdout=subprocess.PIPE)
+
+while True:
+    user_input = input("Currently Reading, Enter 1 to stop reading: ")
+    if user_input == "1":
+        process.kill()
+        stdout = process.communicate()
+        break
+
+
+print(stdout)
+
+
 
